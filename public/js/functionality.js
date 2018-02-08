fe75227d65428b9a604c019473691530483dfc41
Hemankur <hemankur@hotmail.com>
2018-02-08 13:42:40 +0530

    Code Cleaning

--- public/js/functionality.js
+++ public/js/functionality.js
@@ -1,12 +1,9 @@
 window.addEventListener("load", init);
 
-var accounts;
 var nodeType = 'geth';
 var address;
 
 function init() {
-  // document.getElementById("submit").disabled = true;
-
   if (typeof web3 !== 'undefined') {
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
@@ -21,11 +18,6 @@ function init() {
     // var provider = document.getElementById('provider_url').value;
     // window.web3 = new Web3(new Web3.providers.HttpProvider(provider));
   }
-
-
-  // var userInput = document.getElementById("userInput");
-  // var submit = document.getElementById("submit");
-  // submit.addEventListener("click", buttonClicked);
 }
 
 function    setWeb3Version() {
@@ -51,14 +43,10 @@ function doGetAccounts(){
     if(error){
       console.log('accounts count returned an error');
     } else{
-      acounts = result;
       console.log(result.length);
       //result[0] gives the current account
       console.log(result[0]);
       address = result[0];
-      if(address!= null){
-        // document.getElementById("submit").disabled = false;
-      }
       //You need to have at least 1 account to proceed
       if(result.length == 0) {
         if(nodeType == 'metamask'){
@@ -77,15 +65,10 @@ function buttonClicked() {
 function retryFetch(){
   console.log(address); 
   if (address == null){
-    //  alert("Unlock MetaMask and press retry")
-     location.reload();
-    //  window.addEventListener("load", init)
-   }
-   else{
+    location.reload();
+  } else{
      //code to continue with the validation process here
-    //  document.getElementById("submit").disabled = false;
-     console.log("chal to ra hai");
-     authenticate(address);
+    authenticate(address);
    }
 }
 
